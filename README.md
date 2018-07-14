43dd014d3ca515b02c6dd78cc3a11923ca29db4a
Amanda Cavallaro <amdcaruso@gmail.com>
2018-07-14 16:54:25 +0100

    fix: Update readme file
    feat: updates README.md
    
    feat: Add output Component
    
    feat: add react-autocomplete
    
    fix: add Output inside of Search Component
    
    fix: Add event handler to Search component
    
    feat: Add Fetch API call from swapi
    
    feat: Add new Output Component
    
    feat: Add new CSS styles
    
    feat: Ensure SEO is a hundred per cent against lighthouse
    
    feat: Add footer
    
    feat: Add magnifier before image loads
    
    fix: Does not search for empty texts
    
    fix: Refactor code
    
    feat: Add untracked files
    
    feat: Add lego image onClick further info feat
    
    feat: Add lego image:hover cursor:pointer
    
    feat: Add project to netlify
    
    fix: Fixes broken link on readme.md
    
    Update README.md
    
    feat: Makes input bar width bigger
    
    fix: Make character description smaller
    
    fix: Rename Output into ShowResults
    
    feat: Improve card CSS styling
    
    feat: add new ShowResults.js

---
+++ README.md
@@ -0,0 +1,38 @@
+# Star Wars Auto Complete Search
+
+### Technologies used
+
+HTML, CSS, Javascript, React
+
+### Demo
+
+A working demo can be found [here](https://star-wars-character-search.netlify.com/).
+
+### How to run
+
+1. Open your terminal
+2. type `npm start`
+3. press return
+4. open it in your browser either on `localhost:3000` or `localhost:5000`
+
+### Steps taken
+
+1.  Go to freecodecamp and review my knowledge of ES6 and React
+2.  Go to the [Swapi API](https://swapi.co) and test the API
+3.  Understand the library by creating queries and reading documentation
+4.  Model with pencil and paper
+5.  Add Create react app to project
+6.  Implement methods
+7.  Style it using CSS
+8.  Improve and Refactor App
+
+### Reference
+
+1.  Background image: https://www.flickr.com/photos/59263516@N08/23995149986
+2.  Star Wars API: http://swapi.co/
+3.  Lea image: http://lego.wikia.com/wiki/Leia_Organa_Solo
+4.  React documentation: https://reactjs.org/docs/
+5.  Fetch API: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+6.  Fetch API:https://www.robinwieruch.de/react-fetching-data/
+7.  CSS Card: https://www.w3schools.com/howto/howto_css_cards.asp
+8.  Magnifier svg icon: https://www.iconfinder.com/icons/126577/find_search_icon
---
+++ src/DisplayCharacter.js
@@ -0,0 +1,51 @@
+import React from "react";
+import magnifier from "./magnifier.svg";
+export default class DisplayCharacter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: "",
+      furtherInfo: false
+    };
+    this.handleFurtherInfo = this.handleFurtherInfo.bind(this);
+  }
+  handleFurtherInfo() {
+    this.setState({ furtherInfo: !this.state.furtherInfo });
+  }
+  componentDidMount() {
+    fetch(`https://api.randomuser.me/?lego`)
+      .then(j => j.json())
+      .then(data => data.results[0].picture.thumbnail)
+      .then(picture => this.setState({ image: picture }));
+  }
+  render() {
+    return (
+      <div>
+        <div className="card">
+          <span id="card-container" className="container">
+            <img
+              alt="lego"
+              className={this.state.image ? "lego" : "magnifier"}
+              src={this.state.image ? this.state.image : magnifier}
+              onClick={this.handleFurtherInfo}
+            />
+            <h3>{this.props.character.name}</h3>
+          </span>
+          {this.state.furtherInfo ? (
+            <div className="furtherInfo">
+              <h4>
+                Height: <span>{this.props.character.height}</span>
+              </h4>
+              <h4>
+                Hair color: <span>{this.props.character.hair_color}</span>
+              </h4>
+              <h4>
+                Eye color: <span>{this.props.character.eye_color}</span>
+              </h4>
+            </div>
+          ) : null}
+        </div>
+      </div>
+    );
+  }
+}
